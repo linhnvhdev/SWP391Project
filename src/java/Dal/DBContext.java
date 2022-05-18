726d2fa548fa4e1e984bcd8e0f9b952a0eee91b6
Huyenthoaisong1 <whydoihavetousefacebook@gmail.com>
2022-05-18 23:40:34 +0700

    DUong code

--- src/java/Dal/DBContext.java
+++ src/java/Dal/DBContext.java
@@ -22,7 +22,7 @@ public class DBContext {
         try {
             String username = "SE1611";
             String password = "12345";   
-            String url = "jdbc:sqlserver://DESKTOP-9DHDRQ5\\\\SQLEXPRESS:1433;databaseName=Test";
+            String url = "jdbc:sqlserver://DESKTOP-9DHDRQ5:1433;databaseName=SWP391Project";
             //String url = "jdbc:sqlserver://DESKTOP-MVAK160:1433;databaseName=SWP391Project";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
